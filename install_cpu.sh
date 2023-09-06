3d1fb987d67022985fa1d3841bb264bd25960a7a
tknott95 <tknott95@hotmail.com>
2023-09-06 14:57:43 -0600

    install_cpu.sh

---
+++ install_cpu.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+pkgs=(
+  "huggingface_hub" 
+  "python-dotenv" "openai"
+  "torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu"
+  "bitsandbytes" 
+  "gradio"
+  "accelerate>=0.20.3"
+  "transformers==4.30"
+  "flask"
+  # transformers needs to be to be 4.30 for now
+  # "git+https://github.com/huggingface/transformers"
+)  
+
+for pkg in "${pkgs[@]}"; do
+  echo -e "\n\x1B[96m  INSTALLING:   $pkg \x1B[0m"
+  pip install $pkg
+done
